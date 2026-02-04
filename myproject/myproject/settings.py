496d7b19be527e029afa152d617301c93f0879d4
Arjun <arjuni5acer12@gmail.com>
2026-02-05 00:07:55 +0530

    latest

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fake-key-change-in-pr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['django-crud-uay4.onrender.com']
+
 
 # Application definition
 INSTALLED_APPS = [
